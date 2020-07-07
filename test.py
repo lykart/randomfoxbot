31ccd7904f1b83be10df6e361cb8998c1bb9fee6
Nikita <nikza22.01@gmail.com>
2020-07-07 21:02:42 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+api_id = int(os.environ['api_id'])
+api_hash = os.environ["api_hash"]
+bot_token = os.environ["bot_token"]
+ytApiKey = os.environ["ytApiKey"]
+
+print(api_id, api_hash, bot_token, ytApiKey)
+
+
