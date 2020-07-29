7658cfb32d98946f6c283b5139dbfa873d637345
Nikita <nikza22.01@gmail.com>
2020-07-29 17:41:32 +0500

    Brand new interface

--- mainAIOGRAM.py
+++ mainAIOGRAM.py
@@ -19,6 +19,7 @@ from time import time
 import os
 import re
 
+# https://sarratus.imgbb.com/ <- Картинки хранятся здесь
 
 # Инициализация работы бота
 bot_token = os.environ["bot_token"]
@@ -29,39 +30,131 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 
-helpList = [
+helpImages = [
+	'https://i.ibb.co/0Ydh6nV/qm.png',  # Вопросительный знак
+
+	'https://i.ibb.co/T1Mwz1z/H.png',   # H
+
+	'https://i.ibb.co/hDwK85z/E.png',   # E
+
+	'https://i.ibb.co/L6nTC1v/L.png',   # L
+
+	'https://i.ibb.co/vHzW7Rk/P.png'    # P
+]
+
+
+helpList = \
+[
 	[
-		'Введите математическое выражение, оканчивающееся на \"=\"',
+		'Введите один из запросов ниже:',
+
+		'',
+
+		'',
+
+		'Основная функциональность бота заключена в inline-командах. '
+		'Команды можно вводить как строчными, так и заглавными буквами. '
+		'Некоторые из них описаны здесь'
+	], [
+		'Решение примеров:',
+
+		'Математическое выражение c \"=\" на конце',
+
+		'',
 
 		"Введите математическое выражение, состоящее из чисел и простейших операций "
 		"(Лис ещё только учится!!) типа \"*\", \"/\", \"+\", \"-\", оканчивающееся на знак \"=\" "
 		"и Лис посчитает в столбик результат вычислений"
 	], [
-		'Введите \"wiki\" для получения случайной статьи',
+		'Случайная статья из Wikipedia:',
+
+		'\"Wiki\"',
+
+		'https://i.ibb.co/S6mcw2F/1200px-Wikipedia-logo-svg-svg.png',
 
 		"Введите \"wiki\" или \"wikipedia\" в любом регистре, и Лис быстро сбегает до Вики и "
 		"принесёт ссылку на случайную статью из русскоязычной Википедии"
 	], [
-		'Введите вопрос, и бот ответит Да\Нет',
+		'Ответ на вопрос - \"Да\Нет\":',
+
+		'Вопрос, заканчивающийся на \"?\"',
+
+		'',
 
 		"Введите любой вопрос, не содержащий \"или\" (желательно, на который можно ответить "
 		"односложно, Лис ещё плохо понимает человечий и не может изъяснятся без переводчика, "
 		"для большего погружения О_о), и бот ответит на него Да или Нет"
 	], [
-		'Введите список слов через \"или\", и бот выберет одно',
+		'Выбор одного из вариантов:',
+
+		'Список слов через \"или\"',
+
+		'',
 
 		"Введите любое количество слов через \"или\" и Лис, обнюхав, выберет одно из них"
 	], [
-		'Введите \"rate\" и после то, что должен оценить бот',
+		'Оценка чего-либо:',
+
+		'\"rate\" и после то, что должен оценить бот',
+
+		'',
 
 		"Введите \"rate\" в любом регистре и через пробел то, что хотите отдать на оценку Лису. "
 		"Он ответит вам в десятибальной шкале с небольшим милым пояснением"
 	], [
-		'Введите \"yt\" для получения случайного видео'
+		'Случайного видео из YouTube:',
+
+		'\"yt\" или \"youtube\"',
+
+		'https://i.ibb.co/RDttyBT/youtube-logo-png-2069.png',
 
 		"Введите \"yt\" или \"youtube\", чтобы получить случайное видео с ютуба, которое в данный "
 		"момент просматривает Лис"
-	]]
+	], [
+		'QR-код:',
+
+		'\"qr\" и после то, что вы хотите зашифровать',
+
+		'',
+
+		'Введите \"qr\" и то, что вы хотите видеть контентом qr-кода. Бот быстро и со вкусом создаст его'
+	], [
+		'Или в диалоге с ботом:',
+
+		'',
+
+		'',
+
+		'Напишите в личные сообщения боту по тэгу @rnfoxbot или по ссылке t.me/rnfoxbot и введите одну '
+		'из следующих команд'
+	], [
+		'Создание демотиватора',
+
+		'\\демотиватор или \\демо, или \\demotivator',
+
+		'',
+
+		'Бот создаст демотиватор из картинки и двух строк текста, которые вы ему отправите. '
+		'Все взаимодействия с ботом сопровождаются подсказками, так, что вы не потеряетесь'
+	],
+	# [
+	# 	'Или в диалоге с ботом:',
+	#
+	# 	'',
+	#
+	# 	'',
+	#
+	# 	''
+	# ], [
+	# 	'Или в диалоге с ботом:',
+	#
+	# 	'',
+	#
+	# 	'',
+	#
+	# 	''
+	# ]
+]
 
 
 @dp.inline_handler(regexp=r'(?i)^help$|^\s*$')
@@ -70,10 +163,13 @@ async def helpInlineHandler(inline_query: InlineQuery):
 		InlineQueryResultArticle(
 			id=str(i),
 			title=helpList[i][0],
-			input_message_content=InputTextMessageContent(helpList[i][1]))
-		for i in range(len(helpList) - 1)
+			description=helpList[i][1],
+			thumb_url=helpImages[i % 5],
+			input_message_content=InputTextMessageContent(helpList[i][-1]))
+		for i in range(len(helpList))
 	]
-	await bot.answer_inline_query(inline_query.id, results=items, cache_time=300)
+
+	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
 
 
 @dp.inline_handler(regexp=r'(?i)^qr\b.+$')
@@ -104,7 +200,7 @@ async def qrInlineHandler(inline_query: InlineQuery):
 		)
 	]
 
-	await bot.answer_inline_query(inline_query.id, results=items, cache_time=500)
+	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
 
 
 @dp.chosen_inline_handler(lambda chosen_inline_query: re.search(r"(?i)^qr\b.+$", chosen_inline_query.query))
@@ -148,6 +244,8 @@ async def whoIAmInlineHandler(inline_query: InlineQuery):
 		InlineQueryResultArticle(
 			id=str(time()),
 			title='Кто я из суперсемейки?',
+			description=answer,
+			thumb_url='https://i.ibb.co/PmrJZxc/1280px-Gay-Pride-Flag-svg.png',
 			input_message_content=InputTextMessageContent(f"Вы {answer}!"))
 	]
 	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
@@ -156,43 +254,58 @@ async def whoIAmInlineHandler(inline_query: InlineQuery):
 # Обработчик ответа Да\Нет на вопрос Inline Query
 @dp.inline_handler(regexp=r'(?i)^(?=.*?\?)((?!или|or).)*$')
 async def questionInlineQueryHandler(inline_query: InlineQuery):
-	messToUser = markdown.bold(inline_query.query) + '\n' + markdown.italic(yesOrNot())
+	answer = yesOrNot()
+	messToUser = markdown.bold(inline_query.query) + '\n' + markdown.italic(answer)
 	print(messToUser)
+
 	items = [
 		InlineQueryResultArticle(
 			id=str(time()),
-			title='Да|Нет',
+			title='Ответ:',
+			description=answer,
+			thumb_url='https://i.ibb.co/0tzywHx/Unt22itled-1.png',
 			input_message_content=InputTextMessageContent(messToUser, parse_mode='MarkdownV2'))
 	]
+
 	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
 
 
 @dp.inline_handler(regexp=r'(?i)(.+\bили|or\b.+)+')
 async def OrInlineQueryHandler(inline_query: InlineQuery):
-	messToUser = markdown.bold(inline_query.query) + \
-	             '\n' + markdown.italic(orDecider(inline_query.query).capitalize())
+	answer = orDecider(inline_query.query).capitalize()
+	messToUser = markdown.bold(inline_query.query) + '\n' + markdown.italic(answer)
 
 	items = [
 		InlineQueryResultArticle(
 			id=str(time()),
-			title='Одно из слов',
+			title='Одно из слов:',
+			description=answer,
+			thumb_url='https://i.ibb.co/0tzywHx/Unt22itled-1.png',
 			input_message_content=InputTextMessageContent(messToUser, parse_mode='MarkdownV2'))
 	]
+
 	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
 
 
 # Обработчик запроса случайной роры Inline Query
-@dp.inline_handler(regexp=r'(?i)popa|попа\s+\d+')
+@dp.inline_handler(regexp=r'(?i)^\s*popa|попа\s+\d+')
 async def popaInlineQueryHandler(inline_query: InlineQuery):
-	num = int(inline_query.query.lower().replace("popa", "").strip())
+	_str = inline_query.query
+	_str = re.sub(r'(?i)popa|попа', '', _str)
+	num = int(_str.strip())
 
-	messToUser = markdown.bold(randomPopGenerator(num).strip().capitalize())
+	answer = randomPopGenerator(num).strip().capitalize()
+	messToUser = markdown.bold(answer)
+
 	items = [
 		InlineQueryResultArticle(
 			id=str(time()),
-			title='Ророчка',
+			title='Ророчка:',
+			description=answer,
+			thumb_url='https://i.ibb.co/0tzywHx/Unt22itled-1.png',
 			input_message_content=InputTextMessageContent(messToUser, parse_mode='MarkdownV2'))
 	]
+
 	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
 
 
@@ -201,55 +314,75 @@ async def popaInlineQueryHandler(inline_query: InlineQuery):
 async def randNumInlineQueryHandler(inline_query: InlineQuery):
 	num = [int(i) for i in inline_query.query.lower().replace("num", "").split()]
 
-	messToUser = markdown.italic(f"Случайное число от {num[0]} до {num[1]}:") + '\n' + markdown.bold(f'{randint(num[0], num[1])}')
+	randomNumber = str(randint(num[0], num[1]))
+	messToUser = markdown.italic(f"Случайное число от {num[0]} до {num[1]}:") + '\n' + \
+	             markdown.bold(f'{randomNumber}')
 	items = [
 		InlineQueryResultArticle(
 			id=str(time()),
-			title=f'Случайное число от {num[0]} до {num[1]}',
+			title=f'Случайное число от {num[0]} до {num[1]}:',
+			description=randomNumber,
+			thumb_url='https://i.ibb.co/0tzywHx/Unt22itled-1.png',
 			input_message_content=InputTextMessageContent(messToUser, parse_mode='MarkdownV2'))
 	]
+
 	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
 
 
 # Обработчик простейших математических выражений Inline Query
 @dp.inline_handler(regexp=r'[\s\d\.\,\/\*\-\+\(\)]+=$')
 async def calculationInlineQueryHandler(inline_query: InlineQuery):
-	messToUser = markdown.code(inline_query.query.replace(" ", "")) + markdown.code(
-		str(eval(inline_query.query.replace("=", "").replace(",", "."))))
+	answer = str(eval(inline_query.query.replace("=", "").replace(",", ".")))
+	messToUser = markdown.code(inline_query.query.replace(" ", "")) + markdown.code(answer)
+
 	items = [
 		InlineQueryResultArticle(
 			id=str(time()),
-			title='Результат вычислений',
+			title='Результат вычислений:',
+			thumb_url='https://i.ibb.co/QmWSC1N/Untitled-2.png',
+			description=answer,
 			input_message_content=InputTextMessageContent(messToUser, parse_mode='MarkdownV2'))
 	]
-	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
+
+	await bot.answer_inline_query(inline_query.id, results=items, cache_time=50000)
 
 
-@dp.inline_handler(regexp=r'(?i)^\s*gay$')
+@dp.inline_handler(regexp=r'(?i)^\s*gay\b.+')
 async def howMuchInlineQueryHandler(inline_query: InlineQuery):
+	answer = randint(0, 100)
+
+	_str = inline_query.query
+	_str = re.sub(r'(?i)\s*gay\b\s+', '', _str)
+
+	messToUser = markdown.italic(f"{_str} гей на {answer}%")
+
 	items = [
 		InlineQueryResultArticle(
 			id=str(time()),
-			title='Насколько я гей?',
-			input_message_content=InputTextMessageContent(markdown.italic(f"Я Гей на {randint(0, 100)}%"),
-			                                              parse_mode='MarkdownV2'))
+			title=f'Насколько {_str} гей?',
+			description="(the pidor)",
+			thumb_url='https://i.ibb.co/PmrJZxc/1280px-Gay-Pride-Flag-svg.png',
+			input_message_content=InputTextMessageContent(message_text=messToUser,	parse_mode='MarkdownV2'))
 	]
-	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
+
+	await bot.answer_inline_query(inline_query.id, results=items, cache_time=9999999999)
 
 
 # Обработчик запроса оценки Inline Query
 @dp.inline_handler(regexp=r'(?i)rate\b\b.*')
 async def RateInlineQueryHandler(inline_query: InlineQuery):
 	item = inline_query.query.lower().replace("rate", '').strip()
-	print(item)
 	messToUser = randomRating(item)
-	print(messToUser)
+
 	items = [
 		InlineQueryResultArticle(
 			id=str(time()),
 			title='Оценка от Лиса',
+			description='Лисик усердно обнюхал то, что вы принесли ему на оценку, и вынес окончательный вердикт!',
+			thumb_url='https://i.ibb.co/0tzywHx/Unt22itled-1.png',
 			input_message_content=InputTextMessageContent(messToUser, parse_mode='MarkdownV2'))
 	]
+
 	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
 
 
@@ -261,8 +394,11 @@ async def youtubeInlineQueryHandler(inline_query: InlineQuery):
 		InlineQueryResultArticle(
 			id=str(time()),
 			title='Случайное видео с YouTube',
+			description='Рекомендация от Лиса',
+			thumb_url='https://i.ibb.co/RDttyBT/youtube-logo-png-2069.png',
 			input_message_content=InputTextMessageContent(randomYoutubeVideo))
 	]
+
 	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
 
 
@@ -274,8 +410,11 @@ async def wikiInlineQueryHandler(inline_query: InlineQuery):
 		InlineQueryResultArticle(
 			id=str(time()),
 			title='Случайная статья из Википедии',
+			description='Рекомендация от Лиса',
+			thumb_url='https://i.ibb.co/S6mcw2F/1200px-Wikipedia-logo-svg-svg.png',
 			input_message_content=InputTextMessageContent(randomWikiArticle))
 	]
+
 	await bot.answer_inline_query(inline_query.id, results=items, cache_time=0)
 
 
@@ -289,9 +428,9 @@ async def wikiInlineQueryHandler(inline_query: InlineQuery):
 
 # States
 class Form(StatesGroup):
-	pic = State()  # Will be represented in storage as 'Form:name'
-	header = State()  # Will be represented in storage as 'Form:age'
-	subtitle = State()  # Will be represented in storage as 'Form:gender'
+	pic = State()
+	header = State()
+	subtitle = State()
 
 
 @dp.message_handler(filters.RegexpCommandsFilter(regexp_commands=[r'(?i)demotivator|demo|демо|демотиватор$']))
@@ -301,22 +440,6 @@ async def DemotivatorInlineQueryHandler(message: types.Message):
 
 	await Form.pic.set()
 	await message.answer("Отправь картинку, которую хотел бы видеть в демотиваторе", reply_markup=markup)
-
-
-# You can use state '*' if you need to handle all states
-@dp.message_handler(state='*', commands='отмена|cancel')
-@dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
-async def cancel_handler(message: types.Message, state: FSMContext):
-	current_state = await state.get_state()
-	if current_state is None:
-		return
-
-	async with state.proxy() as data:
-		if len(data['pic']) > 2:
-			os.remove(data['pic'])
-
-	await state.finish()
-	await message.reply('Отменено.', reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(content_types=['photo'], state=Form.pic)
@@ -369,6 +492,23 @@ async def process_gender_invalid(message: types.Message, state: FSMContext):
 	os.remove(data['pic'])
 
 	await state.finish()
+
+
+# You can use state '*' if you need to handle all states
+@dp.message_handler(state='*', commands='отмена|cancel')
+@dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+	current_state = await state.get_state()
+	if current_state is None:
+		return
+
+	async with state.proxy() as data:
+		if len(data['pic']) > 2:
+			os.remove(data['pic'])
+
+	logging.info('Cancelling state %r', current_state)
+	await state.finish()
+	await message.reply('Отменено.', reply_markup=types.ReplyKeyboardRemove())
 
 
 ########################## FSM для генерации демотиваторов #############################
